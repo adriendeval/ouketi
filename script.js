ad7a369e81a19a8f0b0cd5685a3fc2dde336f91f
adriendeval <adrien.deval@ikmail.com>
2025-09-19 10:04:25 +0200

    add indexes

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 var tousLesMotsCles = ["Plastique", "Métal", "Carton", "Verre", "Outil", "Rangement", "Jaune", "Rouge", "Bleu", "Vert"];
 var motsClesRestants = [];
 var motsClesSelectionnes = [];
-var cptClics = 0;
-var ordreSelection = {}; // Objet pour stocker l'ordre de sélection
 
 // Éléments de l'interface
 var debug;
@@ -13,7 +11,7 @@ var divMotsClesSelectionnes;
 // Code de la fonction writeln
 function writeln(message) {
     if (debug) {
-        debug.innerHTML += message + "<br>";
+        debug.innerHTML += message + "\n";
     }
 }
 
@@ -33,108 +31,74 @@ function bodyOnLoad() {
 
 // Initialisation les tableaux des boutons
 function init() {
+    // initialiser les tableaux des boutons
+    motsClesRestants = [];
+    motsClesSelectionnes = [];
+
     for (var i = 0; i < tousLesMotsCles.length; i++) {
         motsClesRestants[i] = tousLesMotsCles[i];
     }
+
+    // test...
+    motsClesSelectionnes.push(motsClesRestants[1]);
+    motsClesRestants.splice(1, 1);
 }
 
 function afficherMotsClesRestants() {
-    // Ne vider que si nécessaire
-    let boutonsExistants = divMotsClesRestants.querySelectorAll('.motCle');
-    if (boutonsExistants.length !== motsClesRestants.length) {
-        // Supprimer seulement les boutons, pas le titre
-        boutonsExistants.forEach(btn => btn.remove());
-
-        // Créer le titre s'il n'existe pas
-        if (!divMotsClesRestants.querySelector('span')) {
-            let titre = document.createElement("span");
-            titre.innerText = "Mots-clés restants : ";
-            divMotsClesRestants.appendChild(titre);
-        }
-
-        // Ajouter les nouveaux boutons
-        for (let motCle of motsClesRestants) {
-            let button = document.createElement("button");
-            button.className = "motCle";
-            button.innerText = motCle;
-            button.style.cursor = "pointer";
-            button.style.margin = "5px";
-            button.onclick = function () {
-                deplacerVersSelectionnes(motCle);
-            };
-            divMotsClesRestants.appendChild(button);
+    //En fonction du contenu de motsClesRestants
+    //Réafficher le contenu du div pour les mots clés restants
+    //Générer un bouton pour chaque mot clé
+    divMotsClesRestants.innerHTML = 'Mots Clés : ';
+    for (var i = 0; i < motsClesRestants.length; i++) {
+        var motCle = motsClesRestants[i];
+        if (motCle != null) {
+            const bouton = document.createElement("button");
+            bouton.textContent = motCle;
+            bouton.dataset.idx = i;
+            bouton.dataset.source = 'restants';
+            bouton.onclick = buttonOnClick;
+            divMotsClesRestants.appendChild(bouton);
         }
     }
 }
 
 function afficherMotsClesSelectionnes() {
-    divMotsClesSelectionnes.innerHTML = "";
-    let titre = document.createElement("span");
-    titre.innerText = "Mots-clés sélectionnés : ";
-    divMotsClesSelectionnes.appendChild(titre);
-
-    // Trier les mots-clés sélectionnés par ordre de sélection
-    var motsClesTriés = motsClesSelectionnes.slice().sort(function (a, b) {
-        return ordreSelection[a] - ordreSelection[b];
-    });
-
-    for (let motCle of motsClesTriés) {
-        let button = document.createElement("button");
-        button.className = "motCle";
-        button.innerText = motCle;
-        button.style.cursor = "pointer";
-        button.style.margin = "5px";
-        button.onclick = function () {
-            deplacerVersRestants(motCle);
-        };
-        divMotsClesSelectionnes.appendChild(button);
-    }
-}
-
-// Déplacer un mot-clé des restants vers les sélectionnés
-function deplacerVersSelectionnes(motCle) {
-    // Trouver l'index du mot-clé dans les restants
-    var index = -1;
-    for (var i = 0; i < motsClesRestants.length; i++) {
-        if (motsClesRestants[i] === motCle) {
-            index = i;
-            break;
-        }
-    }
-
-    // Si trouvé, le déplacer
-    if (index !== -1) {
-        cptClics++;
-        ordreSelection[motCle] = cptClics;
-        motsClesSelectionnes.push(motsClesRestants[index]);
-        motsClesRestants.splice(index, 1);
-        afficherMotsClesRestants();
-        afficherMotsClesSelectionnes();
-        debug.value += "Clic " + cptClics + "\n";
-        debug.scrollTop = debug.scrollHeight;
-    }
-}
-
-// Déplacer un mot-clé des sélectionnés vers les restants
-function deplacerVersRestants(motCle) {
-    // Trouver l'index du mot-clé dans les sélectionnés
-    var index = -1;
+    //En fonction du contenu de motsClesSelectionnes
+    //Réafficher le contenu du div pour les mots clés sélectionnés
+    //Générer un bouton pour chaque mot clé
+    divMotsClesSelectionnes.innerHTML = 'Mots Choisis : ';
     for (var i = 0; i < motsClesSelectionnes.length; i++) {
-        if (motsClesSelectionnes[i] === motCle) {
-            index = i;
-            break;
+        var motCle = motsClesSelectionnes[i];
+        if (motCle != null) {
+            const bouton = document.createElement("button");
+            bouton.textContent = motCle;
+            bouton.dataset.idx = i;
+            bouton.dataset.source = 'selectionnes';
+            bouton.onclick = buttonOnClick;
+            divMotsClesSelectionnes.appendChild(bouton);
         }
     }
+}
 
-    // Si trouvé, le déplacer
-    if (index !== -1) {
-        cptClics++;
-        delete ordreSelection[motCle];
-        motsClesRestants.push(motsClesSelectionnes[index]);
-        motsClesSelectionnes.splice(index, 1);
-        afficherMotsClesRestants();
-        afficherMotsClesSelectionnes();
-        debug.value += "Clic " + cptClics + "\n";
-        debug.scrollTop = debug.scrollHeight;
+function buttonOnClick() {
+    var idx = parseInt(this.dataset.idx);
+    var source = this.dataset.source;
+
+    writeln("Clic : " + this.textContent + " (index: " + idx + ")");
+
+    if (source === 'restants') {
+        // Déplacer de restants vers sélectionnés
+        var motCle = motsClesRestants[idx];
+        motsClesSelectionnes.push(motCle);
+        motsClesRestants.splice(idx, 1);
+    } else if (source === 'selectionnes') {
+        // Déplacer de sélectionnés vers restants
+        var motCle = motsClesSelectionnes[idx];
+        motsClesRestants.push(motCle);
+        motsClesSelectionnes.splice(idx, 1);
     }
+
+    // Réafficher les deux divs
+    afficherMotsClesRestants();
+    afficherMotsClesSelectionnes();
 }
