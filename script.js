96b10bdb9ba83b07e156763c7e84be541210a1f8
adriendeval <adrien.deval@ikmail.com>
2025-09-23 17:56:16 +0200

    add things

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
-// Tableaux des boutons de l'application
 var tousLesMotsCles = ["Plastique", "Métal", "Carton", "Verre", "Outil", "Rangement", "Jaune", "Rouge", "Bleu", "Vert"];
 var motsClesRestants = [];
 var motsClesSelectionnes = [];
 
-// Éléments de l'interface
 var debug;
 var divMotsClesRestants;
 var divMotsClesSelectionnes;
 
-// Code de la fonction writeln
 function writeln(message) {
     if (debug) {
         debug.innerHTML += message + "\n";
     }
 }
 
-// Déclenchée une seule fois au chargement de la page
 function bodyOnLoad() {
-
-    // Réifier les éléments de l'interface
     debug = document.getElementById("debug");
     divMotsClesRestants = document.getElementById("motsClesRestants");
     divMotsClesSelectionnes = document.getElementById("motsClesSelectionnes");
 
-    // Initialiser le mécanisme spécifique de l'application
     init();
     afficherMotsClesRestants();
     afficherMotsClesSelectionnes();
 }
 
-// Initialisation les tableaux des boutons
 function init() {
-    // Initialiser les tableaux des boutons
     motsClesRestants = [];
     motsClesSelectionnes = [];
 
     for (var i = 0; i < tousLesMotsCles.length; i++) {
         motsClesRestants[i] = tousLesMotsCles[i];
     }
-
-    // // Test (à supprimer plus tard)
-    // motsClesSelectionnes.push(motsClesRestants[1]);
-    // motsClesRestants.splice(1, 1);
 }
 
 function afficherMotsClesRestants() {
-    // En fonction du contenu de motsClesRestants
-    // Réafficher le contenu du div pour les mots clés restants
-    // Générer un bouton pour chaque mot clé
-    divMotsClesRestants.innerHTML = 'Mots Clés : ';
+    divMotsClesRestants.innerHTML = '<abbr title="Double-cliquez pour réinitialiser">Mots Clés</abbr> : ';
+
+    var abbrElement = divMotsClesRestants.querySelector('abbr');
+    abbrElement.ondblclick = function () {
+        writeln("Mots-clés réinitialisés");
+        init();
+        afficherMotsClesRestants();
+        afficherMotsClesSelectionnes();
+    };
+
     for (var i = 0; i < motsClesRestants.length; i++) {
         var motCle = motsClesRestants[i];
         if (motCle != null) {
@@ -62,9 +55,6 @@ function afficherMotsClesRestants() {
 }
 
 function afficherMotsClesSelectionnes() {
-    // En fonction du contenu de motsClesSelectionnes
-    // Réafficher le contenu du div pour les mots clés sélectionnés
-    // Générer un bouton pour chaque mot clé
     divMotsClesSelectionnes.innerHTML = 'Mots Choisis : ';
     for (var i = 0; i < motsClesSelectionnes.length; i++) {
         var motCle = motsClesSelectionnes[i];
@@ -85,19 +75,16 @@ function buttonOnClick() {
     writeln("Clic : " + motCle + " (index: " + idx + ")");
 
     if (motsClesRestants.indexOf(motCle) !== -1) {
-        // Déplacer de "restants" vers "sélectionnés"
         writeln(motsClesRestants.indexOf(motCle));
         motsClesSelectionnes.push(motCle);
         motsClesRestants.splice(idx, 1);
     } else if (motsClesSelectionnes.indexOf(motCle) !== -1) {
-        // Déplacer de "sélectionnés" vers "restants"
         writeln(motsClesSelectionnes.indexOf(motCle));
         motsClesRestants.push(motCle);
         motsClesSelectionnes.splice(idx, 1);
-        
+
     }
 
-    // Mettre à jour les deux DIVs
     afficherMotsClesRestants();
     afficherMotsClesSelectionnes();
 }
