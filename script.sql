be3693307780cbd2e8b6b552494ccc1867c47ffd
adriendeval <adrien.deval@ikmail.com>
2025-10-02 10:05:06 +0200

    add few sql requests

---
+++ script.sql
@@ -0,0 +1,78 @@
+-- Supprimer la base si elle existe déjà
+DROP DATABASE IF EXISTS Ouketi;
+
+-- Créer la base de données
+CREATE DATABASE Ouketi;
+
+DEFAULT CHARACTER SET utf8mb4;
+
+DEFAULT COLLATE utf8mb4_general_ci;
+
+-- Utiliser la base de données
+USE Ouketi;
+
+-- Créer la table MotsCles
+CREATE TABLE MotsCles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(20) NOT NULL
+);
+
+-- Insérer des mots-clés dans la table MotsCles
+INSERT INTO
+    MotsCles (libelle)
+VALUES
+    ('Plastique'),
+    ('Métal'),
+    ('Carton'),
+    ('Verre'),
+    ('Outil'),
+    ('Rangement'),
+    ('Jaune'),
+    ('Rouge'),
+    ('Bleu'),
+    ('Vert');
+
+
+-- Créer la table Objet
+CREATE TABLE Objet (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nom VARCHAR(20) NOT NULL,
+    quantite INT NOT NULL DEFAULT 1,
+    estConteneur BOOLEAN NOT NULL DEFAULT FALSE,
+    estContenuDans INT,
+    FOREIGN KEY (estContenuDans) REFERENCES Objet(id)
+);
+
+-- Insérer des objets dans la table Objet
+INSERT INTO
+    Objet (nom, estConteneur, estContenuDans)
+VALUES
+    ('Boîte-1', TRUE, NULL),
+    ('Tournevis', FALSE, 1),
+    ('Scie', FALSE, 1);
+
+-- Créer la table Correspond
+CREATE TABLE Correspond (
+    idObjet INT NOT NULL,
+    idMotCle INT NOT NULL,
+    PRIMARY KEY (idObjet, idMotCle),
+    FOREIGN KEY (idObjet) REFERENCES Objet(id),
+    FOREIGN KEY (idMotCle) REFERENCES MotsCles(id)
+);
+
+-- Insérer des correspondances dans la table Correspond
+INSERT INTO
+    Correspond (idObjet, idMotCle)
+VALUES
+    (3, 2),
+    (3, 9),
+    (2, 2),
+    (2, 8),
+    (1, 1);
+
+
+--  garder pour la suite
+SELECT * FROM correspond, motscles, objet WHERE correspond.idObjet = objet.id AND correspond.idMotCle = motscles.id; -- afficher toutes les correspondances
+
+SELECT * FROM correspond WHERE correspond.idMotCle = 8 AND correspond.idObjet IN (SELECT idObjet FROM correspond WHERE correspond.idMotCle = 2); -- afficher les objets en Plastique et Rouge
+
